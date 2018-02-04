58836b0bd947893a4d52164d109a49ebbfe2b723
AppTapp <35783612+AppTapp@users.noreply.github.com>
2018-02-04 02:43:43 +0100

    Fixed a vulnerability in instructions.php

--- instructions.php
+++ instructions.php
@@ -1,3 +1,9 @@
+<?php
+if (!file_exists("config.inc.php"))
+	die("config.inc.php does not exist.");
+
+require_once("config.inc.php");
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
    "http://www.w3.org/TR/html4/loose.dtd">
 
@@ -35,61 +41,60 @@
 	</style>
 </head>
 <body>
-<?php $url = 'http://'.$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>
 <div style="display: block; width: 400px; height: 60px;">&nbsp;</div>
 <div style="margin: 0 auto; display: block; width: 900px;">
 <div class="language">
-	<p>Вы попали на эту страницу, несмотря на то, что она на самом деле является <em>repository</em> для <a href="http://installer5.com/">Installer 5</a>. Это значит, что если Вы добавите адрес <em><?=$url?></em> в Ваш Installer 5, вам будут доступны многие интересные программы. Вот как это сделать:</p>
+	<p>Вы попали на эту страницу, несмотря на то, что она на самом деле является <em>repository</em> для <a href="http://installer5.com/">Installer 5</a>. Это значит, что если Вы добавите адрес <em><?=REPOSITORY_URL?></em> в Ваш Installer 5, вам будут доступны многие интересные программы. Вот как это сделать:</p>
 	<ul>
 		<li>Запустите Installer 5 и перейдите на вкладку Sources.</li>
 		<li>В правом верхнем углу нажмите на кнопку Edit, затем на Add (в левом верхнем углу).</li>
-		<li>В появившемся окошке введите: <strong><?=$url?></strong></li>
+		<li>В появившемся окошке введите: <strong><?=REPOSITORY_URL?></strong></li>
 		<li>Нажмите Done, затем Refresh.</li>
 	</ul>
 </div>
 <div class="language">
-	<p>You've got to this page in your browser, but in fact, it is a <em>repository</em> for <a href="http://installer5.com">Installer 5</a>. This means that if you add the URL <em><?=$url?></em> into  Installer 5, you'll get access to all Installer packages. Here's how:</p>
+	<p>You've got to this page in your browser, but in fact, it is a <em>repository</em> for <a href="http://installer5.com">Installer 5</a>. This means that if you add the URL <em><?=REPOSITORY_URL?></em> into  Installer 5, you'll get access to all Installer packages. Here's how:</p>
 	<ul>
 		<li>Launch Installer 5 and go to the Sources section.</li>
 		<li>In the upper right corner, click Edit, then add (in the upper left corner).</li>
-		<li>In the screen that appears enter: <strong><?=$url?></strong></li>
+		<li>In the screen that appears enter: <strong><?=REPOSITORY_URL?></strong></li>
 		<li>Click Done, then Refresh.</li>
 	</ul>
 </div>
 <div style="clear: both;">&nbsp;</div>
 <div class="language">
-	<p>Εαν έχετε αυτή την σελίδα στον φυλλομετρητή σας, αυτό είναι το <em>repository</em> για το <a href="http://installer5.com">Installer.app</a>.Αυτό σημαίνει ότι αν προσθέσετε το σύνδεσμο <em><?=$url?></em> στο Installer 5, θα έχετε πρόσβαση σε πολύ χρήσιμες εφαρμογές για το iPhone.Ορίστε πώς
+	<p>Εαν έχετε αυτή την σελίδα στον φυλλομετρητή σας, αυτό είναι το <em>repository</em> για το <a href="http://installer5.com">Installer.app</a>.Αυτό σημαίνει ότι αν προσθέσετε το σύνδεσμο <em><?=REPOSITORY_URL?></em> στο Installer 5, θα έχετε πρόσβαση σε πολύ χρήσιμες εφαρμογές για το iPhone.Ορίστε πώς
 		<ul>
 			<li>Ανοίξτε τον Installer 5 και πηγαίνεται στην περιοχή των Sources .Στην πάνω δεξιά γωνία, πατήστε Edit, και μετά add</li>
-			<li>Στην οθόνη που εμφανίζεται δώστε αυτό το σύνδεσμο <strong><?=$url?></strong></li>
+			<li>Στην οθόνη που εμφανίζεται δώστε αυτό το σύνδεσμο <strong><?=REPOSITORY_URL?></strong></li>
 			<li>Πατήστε Done και μετά Refresh</li>
 		</ul>
 </div>
 <div class="language">
-	<p>Está pagina se está mostrando en tu navegador, pero en realidad es un <em>repositorio</em> de <a href="http://installer5.com">Installer 5</a>. Por tanto, si añades esta URL <em><?=$url?></em> en Installer 5, tendrás acceso a unas Cuantas interesantes apps para tu iPhone:</p>
+	<p>Está pagina se está mostrando en tu navegador, pero en realidad es un <em>repositorio</em> de <a href="http://installer5.com">Installer 5</a>. Por tanto, si añades esta URL <em><?=REPOSITORY_URL?></em> en Installer 5, tendrás acceso a unas Cuantas interesantes apps para tu iPhone:</p>
 	<ul>
 		<li>Abre Installer 5 y navega hasta "Sources"</li>
 		<li>En la esquina superior derecha, toca "Edit", después el símbolo + (en la esquina superior izquierda).</li>
-		<li>En la pantalla que aparece introduzca: <strong><?=$url?></strong></li>
+		<li>En la pantalla que aparece introduzca: <strong><?=REPOSITORY_URL?></strong></li>
 		<li>Toque en "Done", después Refresque :-).</li>
 	</ul>
 </div>
 <div style="clear: both;">&nbsp;</div>
 <div class="language">
-  <p>ブラウザでこのページにアクセスしましたが、実際には<a href="http://installer5.com">Installer 5</a>の<em>repository</em>です。つまり、URL <em><?=$url?></em>をInstaller 5に追加すると、アクセス権が取得され、すべてのインストーラパッケージに適用されます。方法は次のとおりです。</p>
+  <p>ブラウザでこのページにアクセスしましたが、実際には<a href="http://installer5.com">Installer 5</a>の<em>repository</em>です。つまり、URL <em><?=REPOSITORY_URL?></em>をInstaller 5に追加すると、アクセス権が取得され、すべてのインストーラパッケージに適用されます。方法は次のとおりです。</p>
   <ul>
     <li>Installer5を実行し、"Sources"タブに移動します。</li>
     <li>右上にある"Edit"ボタンをクリックし、"Add"（左上）をクリックします。</li>
-    <li>表示されるウィンドウで次のように入力します。 <strong><?=$url?></strong></li>
+    <li>表示されるウィンドウで次のように入力します。 <strong><?=REPOSITORY_URL?></strong></li>
     <li>「完了」をクリックし、次に「更新」をクリックします。</li>
   </ul>
 </div>
 <div class="language">
-	<p>Vous êtes arrivé sur cette page dans votre navigateur, mais en fait, il s'agit d'un <em>dépôt</em> pour <a href="http://installer5.com">Installer 5</a>. Cela signifie que si vous ajoutez l'url <em><?=$url?></em> dans Installer 5, vous aurez accès à tous les paquets disponibles. Voici comment faire :</p>
+	<p>Vous êtes arrivé sur cette page dans votre navigateur, mais en fait, il s'agit d'un <em>dépôt</em> pour <a href="http://installer5.com">Installer 5</a>. Cela signifie que si vous ajoutez l'url <em><?=REPOSITORY_URL?></em> dans Installer 5, vous aurez accès à tous les paquets disponibles. Voici comment faire :</p>
 	<ul>
 		<li>Lancer Installer 5 et aller dans la section "Sources".</li>
 		<li>Dans le coin en haut à droite, cliquer sur "Edit", et ensuite "Add" (dans le coin en haut à gauche.</li>
-		<li>Dans l'écran qui apparaît entrer : <strong><?=$url?></strong></li>
+		<li>Dans l'écran qui apparaît entrer : <strong><?=REPOSITORY_URL?></strong></li>
 		<li>Cliquer sur "Done", et rafraîchir.</li>
 	</ul>
 </div>
